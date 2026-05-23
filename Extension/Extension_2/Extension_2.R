d825f84996399fdedba5c7610a4221ebab806c47
Alexiatlg <alexia.leguennic@gmail.com>
2026-05-23 15:13:10 +0200

    i tried to do IRfs with k=4 instead of 12 but it did not solve my stability problem.

--- Extension/Extension_2/Extension_2.R
+++ Extension/Extension_2/Extension_2.R
@@ -679,3 +679,493 @@ lines(horizon, res_back_postcrisis$r_real, lty = 2, lwd = 2)
 abline(h = 0, col = "red")
 
 dev.off()
+
+
+#########################################################
+# Forward-looking Taylor rule: pre/post crisis extension k=12
+#########################################################
+
+# This computes forward-looking Taylor-rule coefficients from the VAR dynamics.
+# Monthly data are used, so k = 12 corresponds to a one-year forward-looking horizon.
+
+compute_forward_coefficients <- function(var_model, k = 12) {
+  
+  # Number of variables in the VAR
+  n_var <- length(var_model$varresult)
+  
+  # Number of lags used in the VAR
+  p_lag <- var_model$p
+  
+  # Size of the companion matrix
+  n_state <- n_var * p_lag
+  
+  # Extract dynamic VAR coefficients only.
+  # This excludes the constant term.
+  A <- sapply(
+    var_model$varresult,
+    function(eq) coef(eq)[1:n_state]
+  ) %>%
+    t()
+  
+  # Build companion matrix.
+  # Top block = estimated VAR dynamics.
+  # Bottom block = mechanical lag shifting.
+  Phi <- rbind(
+    A,
+    cbind(
+      diag(n_state - n_var),
+      matrix(0, n_state - n_var, n_var)
+    )
+  )
+  
+  # Compute average forecast operator:
+  # F_k = (1/k) * sum_{i=1}^k Phi^i
+  Phi_i <- diag(n_state)
+  F_k <- matrix(0, n_state, n_state)
+  
+  for (i in 1:k) {
+    Phi_i <- Phi %*% Phi_i
+    F_k <- F_k + Phi_i
+  }
+  
+  F_k <- F_k / k
+  
+  # Primitive Taylor-rule coefficients
+  # p is already annualized monthly CPI inflation.
+  fp0 <- 1.5
+  fu0 <- 0.5 * (-2.5)
+  
+  # Dynamic effects of current p, u, and r on expected future p and u
+  fp_raw <- fp0 * F_k[1, 1] + fu0 * F_k[2, 1]
+  fu_raw <- fp0 * F_k[1, 2] + fu0 * F_k[2, 2]
+  fr_raw <- fp0 * F_k[1, 3] + fu0 * F_k[2, 3]
+  
+  # Fixed-point correction because current r affects future expectations
+  fp_fwd <- fp_raw / (1 - fr_raw)
+  fu_fwd <- fu_raw / (1 - fr_raw)
+  
+  data.frame(
+    fp_fwd = fp_fwd,
+    fu_fwd = fu_fwd,
+    fr_raw = fr_raw
+  )
+}
+
+#coeffcients
+
+coeff_fwd_precrisis <- compute_forward_coefficients(var_precrisis, k = 12)
+coeff_fwd_postcrisis <- compute_forward_coefficients(var_postcrisis, k = 12)
+
+#NB: diagnostic: check forward-looking coefficients
+
+# If abs(1 - fr_raw) is close to zero, the fixed-point correction is unstable.
+# This can produce implausibly large forward-looking Taylor-rule coefficients.
+# Therefore, forward-looking Taylor-rule IRFs are treated only as exploratory robustness checks.
+coeff_fwd_precrisis
+coeff_fwd_postcrisis
+
+1 - coeff_fwd_precrisis$fr_raw
+1 - coeff_fwd_postcrisis$fr_raw
+
+#observation : in fp_fwd = fp_raw / (1 - fr_raw)  ;  fu_fwd = fu_raw / (1 - fr_raw) the denominator is very close to zero and negative. That mechanically blows up the
+# fixed-point correction => abs(1 - fr_raw) is small, the forward-looking correction is unstable.
+#reason why fu_fwd = 13.55 => not credible for a Taylor-rule unemployment coefficient.
+
+#########################################################
+# Construct forward-looking Taylor-rule residuals
+#########################################################
+
+fp_fwd_pre <- coeff_fwd_precrisis$fp_fwd
+fu_fwd_pre <- coeff_fwd_precrisis$fu_fwd
+
+fp_fwd_post <- coeff_fwd_postcrisis$fp_fwd
+fu_fwd_post <- coeff_fwd_postcrisis$fu_fwd
+
+# Same forward-looking specification as Figure 3:
+# ra = r - fp_fwd*p - fu_fwd*u
+
+taylor_forward_precrisis <- macro_monthly_precrisis %>%
+  mutate(
+    ra = r - fp_fwd_pre * p - fu_fwd_pre * u
+  ) %>%
+  drop_na() %>%
+  dplyr::select(ra, p, u)
+
+taylor_forward_postcrisis <- macro_monthly_postcrisis %>%
+  mutate(
+    ra = r - fp_fwd_post * p - fu_fwd_post * u
+  ) %>%
+  drop_na() %>%
+  dplyr::select(ra, p, u)
+
+  #########################################################
+# Estimate forward-looking Taylor-rule VARs
+#########################################################
+
+var_forw_precrisis <- VAR(
+  y = taylor_forward_precrisis,
+  p = 2,
+  type = "const"
+)
+
+var_forw_postcrisis <- VAR(
+  y = taylor_forward_postcrisis,
+  p = 4,
+  type = "const"
+)
+
+#########################################################
+# IRFs to forward-looking Taylor-rule monetary shock
+#########################################################
+
+irf_forw_precrisis <- irf(
+  var_forw_precrisis,
+  impulse = "ra",
+  response = c("ra", "p", "u"),
+  n.ahead = 24,
+  ortho = TRUE,
+  boot = TRUE,
+  runs = 500,
+  ci = 0.66
+)
+
+irf_forw_postcrisis <- irf(
+  var_forw_postcrisis,
+  impulse = "ra",
+  response = c("ra", "p", "u"),
+  n.ahead = 24,
+  ortho = TRUE,
+  boot = TRUE,
+  runs = 500,
+  ci = 0.66
+)
+
+#########################################################
+# Recover real interest-rate responses
+#########################################################
+
+recover_irf_forward <- function(irf_obj, fp, fu) {
+  
+  ra <- irf_obj$irf$ra[, "ra"]
+  p  <- irf_obj$irf$ra[, "p"]
+  u  <- irf_obj$irf$ra[, "u"]
+  
+  # Figure 3 forward-looking convention:
+  # ra = r - fp*p - fu*u
+  # so r = ra + fp*p + fu*u
+  r_nominal <- ra + fp * p + fu * u
+  
+  # Real interest-rate response
+  r_real <- r_nominal - p
+  
+  list(
+    p = p,
+    u = u,
+    r_real = r_real
+  )
+}
+
+res_forw_precrisis <- recover_irf_forward(
+  irf_forw_precrisis,
+  fp_fwd_pre,
+  fu_fwd_pre
+)
+
+res_forw_postcrisis <- recover_irf_forward(
+  irf_forw_postcrisis,
+  fp_fwd_post,
+  fu_fwd_post
+)
+
+#########################################################
+# Export forward-looking Taylor-rule IRFs
+#########################################################
+
+jpeg(
+  "Extension/Extension_2/Figures/irf_taylor_forward_pre_post.jpeg",
+  width = 1800,
+  height = 1200,
+  res = 150
+)
+
+horizon <- 0:24
+par(mfrow = c(2, 2))
+
+plot(
+  horizon, res_forw_precrisis$p,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis$p, res_forw_postcrisis$p),
+  main = "Inflation response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis$p, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+legend(
+  "topright",
+  legend = c("Pre-crisis", "Post-crisis"),
+  lty = c(1, 2),
+  lwd = 2,
+  bty = "n"
+)
+
+plot(
+  horizon, res_forw_precrisis$u,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis$u, res_forw_postcrisis$u),
+  main = "Unemployment response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis$u, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+
+plot(
+  horizon, res_forw_precrisis$r_real,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis$r_real, res_forw_postcrisis$r_real),
+  main = "Real interest-rate response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis$r_real, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+
+dev.off()
+
+
+#########################################################
+# Optional robustness: forward-looking Taylor rule (k=4)
+#########################################################
+# Not retained in the main analysis because the fixed-point correction remains unstable and produces economically implausible coefficients.
+
+
+compute_forward_coefficients <- function(var_model, k = 4) {
+  
+  n_var <- length(var_model$varresult)
+  p_lag <- var_model$p
+  n_state <- n_var * p_lag
+  
+  # Extract VAR lag coefficients, excluding the constant
+  A <- sapply(
+    var_model$varresult,
+    function(eq) coef(eq)[1:n_state]
+  ) %>%
+    t()
+  
+  # Companion matrix
+  Phi <- rbind(
+    A,
+    cbind(
+      diag(n_state - n_var),
+      matrix(0, n_state - n_var, n_var)
+    )
+  )
+  
+  # Average forecast operator: F_k = (1/k) sum Phi^i
+  Phi_i <- diag(n_state)
+  F_k <- matrix(0, n_state, n_state)
+  
+  for (i in 1:k) {
+    Phi_i <- Phi %*% Phi_i
+    F_k <- F_k + Phi_i
+  }
+  
+  F_k <- F_k / k
+  
+  # Primitive Taylor-rule coefficients
+  fp0 <- 1.5
+  fu0 <- 0.5 * (-2.5)
+  
+  # Forward-looking coefficients
+  fp_raw <- fp0 * F_k[1, 1] + fu0 * F_k[2, 1]
+  fu_raw <- fp0 * F_k[1, 2] + fu0 * F_k[2, 2]
+  fr_raw <- fp0 * F_k[1, 3] + fu0 * F_k[2, 3]
+  
+  fp_fwd <- fp_raw / (1 - fr_raw)
+  fu_fwd <- fu_raw / (1 - fr_raw)
+  
+  data.frame(
+    fp_fwd = fp_fwd,
+    fu_fwd = fu_fwd,
+    fr_raw = fr_raw,
+    denominator = 1 - fr_raw
+  )
+}
+
+#########################################################
+# Compute coefficients
+#########################################################
+
+coeff_fwd_precrisis_k4 <- compute_forward_coefficients(
+  var_precrisis,
+  k = 4
+)
+
+coeff_fwd_postcrisis_k4 <- compute_forward_coefficients(
+  var_postcrisis,
+  k = 4
+)
+
+coeff_fwd_precrisis_k4
+coeff_fwd_postcrisis_k4
+
+#########################################################
+# Construct forward-looking Taylor residuals
+#########################################################
+
+fp_fwd_pre_k4 <- coeff_fwd_precrisis_k4$fp_fwd
+fu_fwd_pre_k4 <- coeff_fwd_precrisis_k4$fu_fwd
+
+fp_fwd_post_k4 <- coeff_fwd_postcrisis_k4$fp_fwd
+fu_fwd_post_k4 <- coeff_fwd_postcrisis_k4$fu_fwd
+
+# Same forward-looking specification as Figure 3:
+# ra = r - fp*p - fu*u
+
+taylor_forward_precrisis_k4 <- macro_monthly_precrisis %>%
+  mutate(
+    ra = r - fp_fwd_pre_k4 * p - fu_fwd_pre_k4 * u
+  ) %>%
+  drop_na() %>%
+  dplyr::select(ra, p, u)
+
+taylor_forward_postcrisis_k4 <- macro_monthly_postcrisis %>%
+  mutate(
+    ra = r - fp_fwd_post_k4 * p - fu_fwd_post_k4 * u
+  ) %>%
+  drop_na() %>%
+  dplyr::select(ra, p, u)
+
+#########################################################
+# Estimate forward-looking Taylor-rule VARs
+#########################################################
+
+var_forw_precrisis_k4 <- VAR(
+  y = taylor_forward_precrisis_k4,
+  p = 2,
+  type = "const"
+)
+
+var_forw_postcrisis_k4 <- VAR(
+  y = taylor_forward_postcrisis_k4,
+  p = 4,
+  type = "const"
+)
+
+#########################################################
+# Compute IRFs
+#########################################################
+
+irf_forw_precrisis_k4 <- irf(
+  var_forw_precrisis_k4,
+  impulse = "ra",
+  response = c("ra", "p", "u"),
+  n.ahead = 24,
+  ortho = TRUE,
+  boot = TRUE,
+  runs = 500,
+  ci = 0.66
+)
+
+irf_forw_postcrisis_k4 <- irf(
+  var_forw_postcrisis_k4,
+  impulse = "ra",
+  response = c("ra", "p", "u"),
+  n.ahead = 24,
+  ortho = TRUE,
+  boot = TRUE,
+  runs = 500,
+  ci = 0.66
+)
+
+#########################################################
+# Recover real interest-rate responses
+#########################################################
+
+recover_irf_forward <- function(irf_obj, fp, fu) {
+  
+  ra <- irf_obj$irf$ra[, "ra"]
+  p  <- irf_obj$irf$ra[, "p"]
+  u  <- irf_obj$irf$ra[, "u"]
+  
+  # Forward-looking convention:
+  # ra = r - fp*p - fu*u
+  # so r = ra + fp*p + fu*u
+  r_nominal <- ra + fp * p + fu * u
+  r_real <- r_nominal - p
+  
+  list(
+    p = p,
+    u = u,
+    r_real = r_real
+  )
+}
+
+res_forw_precrisis_k4 <- recover_irf_forward(
+  irf_forw_precrisis_k4,
+  fp_fwd_pre_k4,
+  fu_fwd_pre_k4
+)
+
+res_forw_postcrisis_k4 <- recover_irf_forward(
+  irf_forw_postcrisis_k4,
+  fp_fwd_post_k4,
+  fu_fwd_post_k4
+)
+
+#########################################################
+# Export forward-looking Taylor-rule IRFs: k = 4
+#########################################################
+
+dir.create(
+  "Extension/Extension_2/Figures",
+  recursive = TRUE,
+  showWarnings = FALSE
+)
+
+jpeg(
+  "Extension/Extension_2/Figures/irf_taylor_forward_pre_post_k4.jpeg",
+  width = 1800,
+  height = 1200,
+  res = 150
+)
+
+horizon <- 0:24
+par(mfrow = c(2, 2))
+
+plot(
+  horizon, res_forw_precrisis_k4$p,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis_k4$p, res_forw_postcrisis_k4$p),
+  main = "Inflation response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis_k4$p, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+legend(
+  "topright",
+  legend = c("Pre-crisis", "Post-crisis"),
+  lty = c(1, 2),
+  lwd = 2,
+  bty = "n"
+)
+
+plot(
+  horizon, res_forw_precrisis_k4$u,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis_k4$u, res_forw_postcrisis_k4$u),
+  main = "Unemployment response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis_k4$u, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+
+plot(
+  horizon, res_forw_precrisis_k4$r_real,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis_k4$r_real, res_forw_postcrisis_k4$r_real),
+  main = "Real interest-rate response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis_k4$r_real, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+
+dev.off()
--- Replication/Code/figure_3.R
+++ Replication/Code/figure_3.R
@@ -166,4 +166,250 @@ abline(h = 0, col = "red")
 
 dev.off()
 
+#diagnostic for myself 
 
+coeff_fwd_precrisis <- compute_forward_coefficients(var_precrisis, k = 4)
+coeff_fwd_postcrisis <- compute_forward_coefficients(var_postcrisis, k = 4)
+
+coeff_fwd_precrisis
+coeff_fwd_postcrisis
+
+1 - coeff_fwd_precrisis$fr_raw
+1 - coeff_fwd_postcrisis$fr_raw
+
+#########################################################
+# Forward-looking Taylor rule: k = 4 robustness check
+#########################################################
+
+compute_forward_coefficients <- function(var_model, k = 4) {
+  
+  n_var <- length(var_model$varresult)
+  p_lag <- var_model$p
+  n_state <- n_var * p_lag
+  
+  # Extract VAR lag coefficients, excluding the constant
+  A <- sapply(
+    var_model$varresult,
+    function(eq) coef(eq)[1:n_state]
+  ) %>%
+    t()
+  
+  # Companion matrix
+  Phi <- rbind(
+    A,
+    cbind(
+      diag(n_state - n_var),
+      matrix(0, n_state - n_var, n_var)
+    )
+  )
+  
+  # Average forecast operator: F_k = (1/k) sum Phi^i
+  Phi_i <- diag(n_state)
+  F_k <- matrix(0, n_state, n_state)
+  
+  for (i in 1:k) {
+    Phi_i <- Phi %*% Phi_i
+    F_k <- F_k + Phi_i
+  }
+  
+  F_k <- F_k / k
+  
+  # Primitive Taylor-rule coefficients
+  fp0 <- 1.5
+  fu0 <- 0.5 * (-2.5)
+  
+  # Forward-looking coefficients
+  fp_raw <- fp0 * F_k[1, 1] + fu0 * F_k[2, 1]
+  fu_raw <- fp0 * F_k[1, 2] + fu0 * F_k[2, 2]
+  fr_raw <- fp0 * F_k[1, 3] + fu0 * F_k[2, 3]
+  
+  fp_fwd <- fp_raw / (1 - fr_raw)
+  fu_fwd <- fu_raw / (1 - fr_raw)
+  
+  data.frame(
+    fp_fwd = fp_fwd,
+    fu_fwd = fu_fwd,
+    fr_raw = fr_raw,
+    denominator = 1 - fr_raw
+  )
+}
+
+#########################################################
+# Compute coefficients
+#########################################################
+
+coeff_fwd_precrisis_k4 <- compute_forward_coefficients(
+  var_precrisis,
+  k = 4
+)
+
+coeff_fwd_postcrisis_k4 <- compute_forward_coefficients(
+  var_postcrisis,
+  k = 4
+)
+
+coeff_fwd_precrisis_k4
+coeff_fwd_postcrisis_k4
+
+#########################################################
+# Construct forward-looking Taylor residuals
+#########################################################
+
+fp_fwd_pre_k4 <- coeff_fwd_precrisis_k4$fp_fwd
+fu_fwd_pre_k4 <- coeff_fwd_precrisis_k4$fu_fwd
+
+fp_fwd_post_k4 <- coeff_fwd_postcrisis_k4$fp_fwd
+fu_fwd_post_k4 <- coeff_fwd_postcrisis_k4$fu_fwd
+
+# Same forward-looking specification as Figure 3:
+# ra = r - fp*p - fu*u
+
+taylor_forward_precrisis_k4 <- macro_monthly_precrisis %>%
+  mutate(
+    ra = r - fp_fwd_pre_k4 * p - fu_fwd_pre_k4 * u
+  ) %>%
+  drop_na() %>%
+  dplyr::select(ra, p, u)
+
+taylor_forward_postcrisis_k4 <- macro_monthly_postcrisis %>%
+  mutate(
+    ra = r - fp_fwd_post_k4 * p - fu_fwd_post_k4 * u
+  ) %>%
+  drop_na() %>%
+  dplyr::select(ra, p, u)
+
+#########################################################
+# Estimate forward-looking Taylor-rule VARs
+#########################################################
+
+var_forw_precrisis_k4 <- VAR(
+  y = taylor_forward_precrisis_k4,
+  p = 2,
+  type = "const"
+)
+
+var_forw_postcrisis_k4 <- VAR(
+  y = taylor_forward_postcrisis_k4,
+  p = 4,
+  type = "const"
+)
+
+#########################################################
+# Compute IRFs
+#########################################################
+
+irf_forw_precrisis_k4 <- irf(
+  var_forw_precrisis_k4,
+  impulse = "ra",
+  response = c("ra", "p", "u"),
+  n.ahead = 24,
+  ortho = TRUE,
+  boot = TRUE,
+  runs = 500,
+  ci = 0.66
+)
+
+irf_forw_postcrisis_k4 <- irf(
+  var_forw_postcrisis_k4,
+  impulse = "ra",
+  response = c("ra", "p", "u"),
+  n.ahead = 24,
+  ortho = TRUE,
+  boot = TRUE,
+  runs = 500,
+  ci = 0.66
+)
+
+#########################################################
+# Recover real interest-rate responses
+#########################################################
+
+recover_irf_forward <- function(irf_obj, fp, fu) {
+  
+  ra <- irf_obj$irf$ra[, "ra"]
+  p  <- irf_obj$irf$ra[, "p"]
+  u  <- irf_obj$irf$ra[, "u"]
+  
+  # Forward-looking convention:
+  # ra = r - fp*p - fu*u
+  # so r = ra + fp*p + fu*u
+  r_nominal <- ra + fp * p + fu * u
+  r_real <- r_nominal - p
+  
+  list(
+    p = p,
+    u = u,
+    r_real = r_real
+  )
+}
+
+res_forw_precrisis_k4 <- recover_irf_forward(
+  irf_forw_precrisis_k4,
+  fp_fwd_pre_k4,
+  fu_fwd_pre_k4
+)
+
+res_forw_postcrisis_k4 <- recover_irf_forward(
+  irf_forw_postcrisis_k4,
+  fp_fwd_post_k4,
+  fu_fwd_post_k4
+)
+
+#########################################################
+# Export forward-looking Taylor-rule IRFs: k = 4
+#########################################################
+
+dir.create(
+  "Extension/Extension_2/Figures",
+  recursive = TRUE,
+  showWarnings = FALSE
+)
+
+jpeg(
+  "Extension/Extension_2/Figures/irf_taylor_forward_pre_post_k4.jpeg",
+  width = 1800,
+  height = 1200,
+  res = 150
+)
+
+horizon <- 0:24
+par(mfrow = c(2, 2))
+
+plot(
+  horizon, res_forw_precrisis_k4$p,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis_k4$p, res_forw_postcrisis_k4$p),
+  main = "Inflation response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis_k4$p, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+legend(
+  "topright",
+  legend = c("Pre-crisis", "Post-crisis"),
+  lty = c(1, 2),
+  lwd = 2,
+  bty = "n"
+)
+
+plot(
+  horizon, res_forw_precrisis_k4$u,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis_k4$u, res_forw_postcrisis_k4$u),
+  main = "Unemployment response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis_k4$u, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+
+plot(
+  horizon, res_forw_precrisis_k4$r_real,
+  type = "l", lwd = 2,
+  ylim = range(res_forw_precrisis_k4$r_real, res_forw_postcrisis_k4$r_real),
+  main = "Real interest-rate response",
+  xlab = "Months", ylab = "Percent"
+)
+lines(horizon, res_forw_postcrisis_k4$r_real, lty = 2, lwd = 2)
+abline(h = 0, col = "red")
+
+dev.off()
